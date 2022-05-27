a3182f2aacd8ee89bb51bcd956c0290f974f1b01
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-05-27 11:11:21 +0530

    Time: 112 ms (42.91%), Space: 13.4 MB (36.81%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        
+        int n=nums.size();
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-      
-       set<vector<int>> sv;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-    
-                for(int k=j+1;k<n;k++)
-                { 
-                  
-                   int x = (long long)target - 
-                           (long long)nums[i]-
-                           (long long)nums[j]-(long long)nums[k];
-                   
-                        if(binary_search(nums.begin()+k+1,nums.end(),x)){
-                            vector<int> v;
-                            v.push_back(nums[i]);
-                            v.push_back(nums[j]);
-                            v.push_back(nums[k]);
-                            v.push_back(x);
-                            sort(v.begin(),v.end());
-                            sv.insert(v);
-                        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int twoSum=target-nums[i]-nums[j];
+                int left=j+1;
+                int right=n-1;
+                while(left<right){
+                    if(nums[left]+nums[right]==twoSum){
+                        vector<int> v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[j]);
+                        v.push_back(nums[left]);
+                        v.push_back(nums[right]);
+                        sort(v.begin(),v.end());
+                        ans.push_back(v);
+                        
+                        while(left<right && nums[left]==v[2]) left++;
+                        while(left<right && nums[right]==v[3]) right--;
+                    }
+                    else if(nums[left]+nums[right]>twoSum) right--;
+                    else left++;
                 }
+                while(j+1<n && nums[j]==nums[j+1]) j++;
             }
+            while(i+1<n && nums[i]==nums[i+1]) i++;
         }
-        vector<vector<int>> res(sv.begin(),sv.end());
-        return res;
+        return ans;
     }
 };
