d006950391f100d38f30d003b271b0a10c8dcbd5
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-05-27 09:50:44 +0530

    Time: 10 ms (90.32%), Space: 10.8 MB (45.79%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // vector<int> ans;
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=i+1;j<nums.size();j++){
-        //         if(nums[i]+nums[j]==target){
-        //             ans.push_back(i);
-        //             ans.push_back(j);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return ans;
-        vector<int> temp;
-        vector<int> ans;
-        temp=nums;
-        sort(temp.begin(),temp.end());
-        int i=0,j=nums.size()-1,n1,n2;
-        while(i<=j){
-            if(temp[i]+temp[j]==target){
-                n1=temp[i];n2=temp[j];
-                break;
-            }
-            else if(temp[i]+temp[j]>target) j--;
-            else i++;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==n1) ans.push_back(i);
-            else if(nums[i]==n2) ans.push_back(i);
-        }
-        return ans;
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         // vector<int> ans;
+//         // for(int i=0;i<nums.size();i++){
+//         //     for(int j=i+1;j<nums.size();j++){
+//         //         if(nums[i]+nums[j]==target){
+//         //             ans.push_back(i);
+//         //             ans.push_back(j);
+//         //             break;
+//         //         }
+//         //     }
+//         // }
+//         // return ans;
         
+//         vector<int> temp;
+//         vector<int> ans;
+//         temp=nums;
+//         sort(temp.begin(),temp.end());
+//         int i=0,j=nums.size()-1,n1,n2;
+//         while(i<=j){
+//             if(temp[i]+temp[j]==target){
+//                 n1=temp[i];n2=temp[j];
+//                 break;
+//             }
+//             else if(temp[i]+temp[j]>target) j--;
+//             else i++;
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==n1) ans.push_back(i);
+//             else if(nums[i]==n2) ans.push_back(i);
+//         }
+//         return ans;
+        
+//     }
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    vector<int> res;
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); ++i) {
+
+   	 if (mp.find(target - nums[i]) != mp.end()) {
+
+   		 res.emplace_back(i);
+   		 res.emplace_back(mp[target - nums[i]]);
+   		 return res;
+   	 }
+
+   	 mp[nums[i]] = i;
     }
+
+    return res;
+}
 };
