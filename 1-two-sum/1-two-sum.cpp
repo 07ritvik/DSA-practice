34b1bb266a67c5fb8e3177dbdab262c966414567
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-05-27 09:48:17 +0530

    Time: 12 ms (83.80%), Space: 10.5 MB (59.64%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<int> ans;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+        vector<int> temp;
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        temp=nums;
+        sort(temp.begin(),temp.end());
+        int i=0,j=nums.size()-1,n1,n2;
+        while(i<=j){
+            if(temp[i]+temp[j]==target){
+                n1=temp[i];n2=temp[j];
+                break;
             }
+            else if(temp[i]+temp[j]>target) j--;
+            else i++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==n1) ans.push_back(i);
+            else if(nums[i]==n2) ans.push_back(i);
         }
         return ans;
+        
     }
 };
