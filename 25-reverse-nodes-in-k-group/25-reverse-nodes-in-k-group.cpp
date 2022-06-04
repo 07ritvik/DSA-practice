07a1356a468c6a3e7acdef6522e4e093dea1b715
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-06-04 15:45:05 +0530

    Time: 30 ms (18.66%), Space: 11.4 MB (95.45%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -11,47 +11,52 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // ListNode* currptr=head;
-        // ListNode* prevptr=NULL;
-        // ListNode* nextptr;
-        // int cnt=0;
-        // int n;
-        // while()
-        // while(currptr!=NULL&&cnt<k){
-        //     nextptr=currptr->next;
-        //     currptr->next=prevptr;
-        //     prevptr=currptr;
-        //     currptr=nextptr;
-        //     cnt++;
-        // }
-        // if(currptr!=NULL){
-        //     head->next=reverseKGroup(nextptr,k);
-        // }
-        // return prevptr;
-        if(head==NULL || head->next==NULL) return head;
-        int length =0;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            length++;
-            temp=temp->next;
+        ListNode* t=head;
+        for(int i=0;i<k;i++){
+            if(t==NULL) return head;
+            t=t->next;
         }
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        ListNode* pre=dummy;
-        ListNode* cur;
-        ListNode* nex;
-        while(length>=k){
-            cur=pre->next;
-            nex=cur->next;
-            for(int i=1;i<k;i++){    // k-1 links change krni padegi
-                cur->next=nex->next;
-                nex->next=pre->next;
-                pre->next=nex;
-                nex=cur->next;
-            }
-            pre=cur;
-            length-=k;
+        ListNode* currptr=head;
+        ListNode* prevptr=NULL;
+        ListNode* nextptr;
+        int cnt=0;
+        int n;
+       
+        while(currptr!=NULL&&cnt<k){
+            nextptr=currptr->next;
+            currptr->next=prevptr;
+            prevptr=currptr;
+            currptr=nextptr;
+            cnt++;
         }
-        return dummy->next;
+        if(currptr!=NULL){
+            head->next=reverseKGroup(nextptr,k);
+        }
+        return prevptr;
+        // if(head==NULL || head->next==NULL) return head;
+        // int length =0;
+        // ListNode* temp=head;
+        // while(temp!=NULL){
+        //     length++;
+        //     temp=temp->next;
+        // }
+        // ListNode* dummy=new ListNode(0);
+        // dummy->next=head;
+        // ListNode* pre=dummy;
+        // ListNode* cur;
+        // ListNode* nex;
+        // while(length>=k){
+        //     cur=pre->next;
+        //     nex=cur->next;
+        //     for(int i=1;i<k;i++){    // k-1 links change krni padegi
+        //         cur->next=nex->next;
+        //         nex->next=pre->next;
+        //         pre->next=nex;
+        //         nex=cur->next;
+        //     }
+        //     pre=cur;
+        //     length-=k;
+        // }
+        // return dummy->next;
     }
 };
