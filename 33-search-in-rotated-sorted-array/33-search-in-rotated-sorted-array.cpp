385154ed6fc1cd9ff9bf0af6505e78636a9db36f
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-07-04 12:43:07 +0530

    Time: 7 ms (51.58%), Space: 11.1 MB (29.10%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int BS(vector<int>&nums,int low,int high,int target){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+    int minInd(vector<int>&nums,int low,int high){
+        int n=nums.size();
+        while(low<=high){
+            if(nums[low]<=nums[high]) return low;
+            int mid=low+(high-low)/2;
+            int prev=(mid-1+n)%n;
+            int next=(mid+1)%n;
+            if(nums[mid]<=nums[prev]&&nums[mid]<=nums[next]) return mid;
+            else if(nums[low]<=nums[mid]) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int ind=minInd(nums,0,nums.size()-1);
+        int a=BS(nums,0,ind-1,target);
+        int b=BS(nums,ind,nums.size()-1,target);
+        if(a!=-1) return a;
+        return b;
+    }
+};
