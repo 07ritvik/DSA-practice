f59a761fae313838671b2627d23af93b7c6115f7
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-08-18 19:30:43 +0530

    Time: 12 ms (11.71%), Space: 12.2 MB (10.65%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,17 +11,25 @@
  */
 class Solution {
 public:
-    void recursion(TreeNode *root, int level, vector<int> &res)
-    {
-        if(root==NULL) return ;
-        if(res.size()<level) res.push_back(root->val);
-        recursion(root->right, level+1, res);
-        recursion(root->left, level+1, res);
-    }
-    
-    vector<int> rightSideView(TreeNode *root) {
-        vector<int> res;
-        recursion(root, 1, res);
-        return res;
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        map<int,int> mp;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            pair<TreeNode*,int> p=q.front();
+            q.pop();
+            TreeNode* frontNode=p.first;
+            int lvl=p.second;
+            mp[lvl]=frontNode->val;
+            if(frontNode->left) q.push({frontNode->left,lvl+1});
+            if(frontNode->right) q.push({frontNode->right,lvl+1});
+        }
+        
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
