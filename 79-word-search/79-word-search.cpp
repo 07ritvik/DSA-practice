d5545d4ce08412764c4a0172e7a6c16cc1d839af
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-08-19 15:41:50 +0530

    Time: 250 ms (94.79%), Space: 8.1 MB (41.02%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -39,20 +39,7 @@
 
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        for(int i = 0; i < board.size(); ++i)
-        {
-            for(int j = 0; j < board[0].size(); ++j)
-            {
-                if(dfs(board, word, i, j, 0))
-                    return true;
-            }
-        }
-        return false;
-    
-    }
-
-    bool dfs(vector<vector<char>>& board, const string& word, int i, int j, int index)
+        bool dfs(vector<vector<char>>& board, const string& word, int i, int j, int index)
     {
         if(index == word.size()) return true; // end condition
         if(i < 0 || j < 0 || i > board.size()-1 || j > board[0].size()-1) return false; // boundary of matrix
@@ -67,4 +54,19 @@ public:
         board[i][j] = word[index]; // modify it back!
         return furtherSearch;
     }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); ++i)
+        {
+            for(int j = 0; j < board[0].size(); ++j)
+            {
+                if(dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    
+    }
+
+
 };
