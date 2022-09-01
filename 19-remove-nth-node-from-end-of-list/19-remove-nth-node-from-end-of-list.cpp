43cf1452a5e88d17c261449017a236db41d1b295
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-09-01 16:04:21 +0530

    Time: 8 ms (44.31%), Space: 10.8 MB (32.63%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,31 +11,48 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp=head;
-        int cnt=1;
-        while(temp!=NULL){
-            temp=temp->next;
-            cnt++;
-        }
-        temp=head;
-        n=cnt-n;
-        cnt=1;
-        if(n==1)return head->next;
-        ListNode* prev=NULL;
-        while(temp!=NULL){
-            if(cnt==n){
-                ListNode* todelete=temp;
-                prev->next=temp->next;
+//         ListNode* temp=head;
+//         int cnt=1;
+//         while(temp!=NULL){
+//             temp=temp->next;
+//             cnt++;
+//         }
+//         temp=head;
+//         n=cnt-n;
+//         cnt=1;
+//         if(n==1)return head->next;
+//         ListNode* prev=NULL;
+//         while(temp!=NULL){
+//             if(cnt==n){
+//                 ListNode* todelete=temp;
+//                 prev->next=temp->next;
                 
-                temp=temp->next;
-                delete(todelete);
-                break;   
-            }
-            cnt++;
-            prev=temp;
-            temp=temp->next;
+//                 temp=temp->next;
+//                 delete(todelete);
+//                 break;   
+//             }
+//             cnt++;
+//             prev=temp;
+//             temp=temp->next;
+//         }
+//         return head;
+        ListNode * start = new ListNode();
+        start -> next = head;
+        ListNode* fast = start;
+        ListNode* slow = start;     
+
+        for(int i = 1; i <= n; ++i)
+            fast = fast->next;
+    
+        while(fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
         }
-        return head;
+        
+        slow->next = slow->next->next;
+        
+        return start->next;        
         
     }
 };
