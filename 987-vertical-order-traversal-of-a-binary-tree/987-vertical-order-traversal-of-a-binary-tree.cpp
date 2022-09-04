c6a4d091c46962db314af2ca7323ad8821a6fa04
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-09-04 16:58:51 +0530

    Time: 3 ms (93.93%), Space: 12.5 MB (78.96%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,29 +13,32 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        map<int,map<int,vector<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> q;
         if(root==NULL) return ans;
-        q.push(make_pair(root,make_pair(0,0)));
+        map<int,map<int,vector<int>>> mp;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+        
         while(!q.empty()){
             pair<TreeNode*,pair<int,int>> temp=q.front();
             q.pop();
             TreeNode* frontNode=temp.first;
-            int hd=temp.second.first;
             int lvl=temp.second.second;
-            nodes[hd][lvl].push_back(frontNode->val);
-            if(frontNode->left) q.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
-            if(frontNode->right) q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+            int hd=temp.second.first;
+            
+            mp[hd][lvl].push_back(frontNode->val);
+            if(frontNode->left) q.push({frontNode->left,{hd-1,lvl+1}});
+            if(frontNode->right) q.push({frontNode->right,{hd+1,lvl+1}});
+            
         }
-
-        for (auto i : nodes) {
+        
+        
+        for(auto i:mp){
             vector<int> v;
-            for (auto j : i.second) {
+            for(auto j:i.second){
                 sort(j.second.begin(),j.second.end());
-               for(auto k:j.second){
-                   
-                   v.push_back(k);
-               }
+                for(auto k:j.second){
+                    v.push_back(k);
+                }
             }
             
             ans.push_back(v);
