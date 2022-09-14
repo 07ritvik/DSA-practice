6fc63e4d6b9cf57fb41b2b255b126d0b0871f4f3
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-09-14 10:58:56 +0530

    Time: 7 ms (56.37%), Space: 9.5 MB (76.54%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -21,24 +21,43 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // vector<int> dp(days.size(),-1);
         // return solve(0,days,costs,dp);
-        int n=days.size();
-        vector<int> dp(n+1,INT_MAX);
-        dp[n]=0;
+//         int n=days.size();
+//         vector<int> dp(n+1,INT_MAX);
+//         dp[n]=0;
         
-        for(int i=n-1;i>=0;i--){
-            int opt1=costs[0] + dp[i+1];
+//         for(int i=n-1;i>=0;i--){
+//             int opt1=costs[0] + dp[i+1];
             
-            int j;
-            for(j=i;j<n&&days[j]<days[i]+7;j++);
-            int opt2=costs[1] + dp[j];
+//             int j;
+//             for(j=i;j<n&&days[j]<days[i]+7;j++);
+//             int opt2=costs[1] + dp[j];
             
             
-            for(j=i;j<n&&days[j]<days[i]+30;j++);
-            int opt3=costs[2] + dp[j];
+//             for(j=i;j<n&&days[j]<days[i]+30;j++);
+//             int opt3=costs[2] + dp[j];
             
-            dp[i]=min({opt1,opt2,opt3});
+//             dp[i]=min({opt1,opt2,opt3});
            
+//         }
+//         return dp[0];
+        
+        int ans=0;
+        
+        queue<pair<int,int>> month;
+        queue<pair<int,int>> week;
+        
+        
+        for(int day: days){
+            while(!month.empty() && month.front().first+30<=day)
+                month.pop();
+            while(!week.empty() && week.front().first+7<=day)
+                week.pop();
+            
+            week.push({day,ans+costs[1]});
+            month.push({day,ans+costs[2]});
+            
+            ans=min(ans+costs[0],min(week.front().second,month.front().second));
         }
-        return dp[0];
+        return ans;
     }
 };
