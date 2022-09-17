29faf332358a3b69256ca2eb95382046f187dd41
07ritvik <66718789+07ritvik@users.noreply.github.com>
2022-09-17 16:57:35 +0530

    Time: 726 ms (39.19%), Space: 188 MB (97.70%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -19,35 +19,71 @@ public:
 //         }
 //     }
     
+    
+// //     Tab
+//             int n=obs.size()-1;
+//         vector<vector<int>> dp(4,vector<int>(obs.size(),INT_MAX));
+        
+//         dp[0][n]=0;
+//         dp[1][n]=0;
+//         dp[2][n]=0;
+//         dp[3][n]=0;
+        
+//         for(int i=n-1;i>=0;i--){
+//             for(int lane=1;lane<=3;lane++){
+                
+//                 if(obs[i+1]!=lane){
+//                     dp[lane][i]=dp[lane][i+1];
+                    
+//                 }
+//                 else{
+//                     int ans=INT_MAX;
+//                     for(int j=1;j<=3;j++){
+//                         if(obs[i]!=j && lane!=j){
+//                          ans=min(ans,1+dp[j][i+1]);   
+//                         }
+//                     }
+//                     dp[lane][i]=ans;
+//                 }
+//             }
+//         }
+//         return min({dp[2][0],1+dp[1][0],1+dp[3][0]});
+    
+    
     int minSideJumps(vector<int>& obs) {
         // vector<vector<int>> dp(4,vector<int>(obs.size(),-1));
         // return solve(0,2,obs,dp);
         int n=obs.size()-1;
-        vector<vector<int>> dp(4,vector<int>(obs.size(),INT_MAX));
+        // vector<vector<int>> dp(4,vector<int>(obs.size(),INT_MAX));
         
-        dp[0][n]=0;
-        dp[1][n]=0;
-        dp[2][n]=0;
-        dp[3][n]=0;
+        vector<int> curr(4,INT_MAX);
+        vector<int> next(4,INT_MAX);
+        
+        next[0]=0;
+        next[1]=0;
+        next[2]=0;
+        next[3]=0;
         
         for(int i=n-1;i>=0;i--){
             for(int lane=1;lane<=3;lane++){
                 
                 if(obs[i+1]!=lane){
-                    dp[lane][i]=dp[lane][i+1];
+                    curr[lane]=next[lane];
                     
                 }
                 else{
                     int ans=INT_MAX;
                     for(int j=1;j<=3;j++){
                         if(obs[i]!=j && lane!=j){
-                         ans=min(ans,1+dp[j][i+1]);   
+                         ans=min(ans,1+next[j]);   
                         }
                     }
-                    dp[lane][i]=ans;
+                    curr[lane]=ans;
                 }
             }
+            next=curr;
         }
-        return min({dp[2][0],1+dp[1][0],1+dp[3][0]});
+        return min({curr[2],1+curr[1],1+curr[3]});
+
     }
 };
